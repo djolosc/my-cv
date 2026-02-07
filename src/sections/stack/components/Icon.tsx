c649eecbb60e2ae75fd8f0949f716f0eaecdf100
djolosc <djsimovic@gmail.com>
2026-02-07 16:18:58 +0100

    FIX: Icon click bug

--- src/sections/stack/components/Icon.tsx
+++ src/sections/stack/components/Icon.tsx
@@ -1,5 +1,5 @@
 import { isDesktopDevice } from "@/utils/device";
-import { useState, type FC } from "react";
+import { useState, useRef, useEffect, type FC } from "react";
 import styled from "styled-components";
 
 interface IconProps {
@@ -10,25 +10,35 @@ interface IconProps {
 
 const Icon: FC<IconProps> = ({ name, backgroundColor, icon }) => {
   const [hovered, setHovered] = useState(false);
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
   const isDesktop = isDesktopDevice();
 
   const handleClick = () => {
     if (isDesktop) return;
+
+    if (timeoutRef.current) clearTimeout(timeoutRef.current);
+
     setHovered(true);
 
-    setTimeout(() => {
+    timeoutRef.current = setTimeout(() => {
       setHovered(false);
     }, 1500);
   };
 
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) clearTimeout(timeoutRef.current);
+    };
+  }, []);
+
   return (
     <StyledWrapper
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
+      onMouseEnter={() => isDesktop && setHovered(true)}
+      onMouseLeave={() => isDesktop && setHovered(false)}
       onClick={handleClick}
     >
-      <IconWrapper key={name} $backgroundColor={backgroundColor}>
+      <IconWrapper $backgroundColor={backgroundColor}>
         <IconImg src={icon} alt={name} />
       </IconWrapper>
       <IconLabel $visible={hovered}>{name}</IconLabel>
@@ -46,11 +56,9 @@ const IconWrapper = styled.div<{ $backgroundColor?: string }>`
   width: 44px;
   height: 44px;
   margin: 0 ${({ theme }) => theme.spacing.s12};
-
   border-radius: ${({ theme }) => theme.radius.r12};
   background-color: ${({ theme, $backgroundColor }) =>
     $backgroundColor || theme.colors.white1};
-
   display: flex;
   align-items: center;
   justify-content: center;
@@ -66,15 +74,13 @@ const IconLabel = styled.span<{ $visible?: boolean }>`
   left: 50%;
   transform: translateX(-50%);
   bottom: -40px;
-
   background: ${({ theme }) => theme.colors.grey3};
   color: ${({ theme }) => theme.colors.white1};
   font-size: ${({ theme }) => theme.fontSizes.fs12};
   padding: 2px 8px;
   border-radius: 6px;
   white-space: nowrap;
-
   opacity: ${({ $visible }) => ($visible ? 1 : 0)};
   pointer-events: none;
-  transition: all 0.25s ease;
+  transition: opacity 0.25s ease;
 `;
